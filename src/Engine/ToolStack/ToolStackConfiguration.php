90769ceed543648a87efda619bec3f071b2964ad
Alain Schlesser <alain.schlesser@gmail.com>
2021-11-12 10:57:17 +0100

    Allow registering of new tools via ToolStackConfiguration

--- src/Engine/ToolStack/ToolStackConfiguration.php
+++ src/Engine/ToolStack/ToolStackConfiguration.php
@@ -13,19 +13,40 @@ final class ToolStackConfiguration
 {
 
     /**
+     * List of tools and their dependencies.
+     *
+     * @var array<class-string<Tool>, array<class-string<Tool>>> Associative array of tools. Key is the tool FQCN,
+     *                                                           value is an array of dependencies.
+     */
+    private $tools = [
+        Tool\AmpOptimizer::class => [],
+        Tool\AmpValidator::class => [],
+        Tool\Lighthouse::class   => [],
+    ];
+
+    /**
      * Get the registered tools and their inter-dependencies.
      *
-     * @return array<class-string<Tool>, array<class-string<Tool>>> Associative array of tools. Key is the tool name,
+     * @return array<class-string<Tool>, array<class-string<Tool>>> Associative array of tools. Key is the tool FQCN,
      *                                                              value is an array of dependencies.
      */
     public function getTools()
     {
         // TODO: Use dynamic configuration data.
 
-        return [
-            Tool\AmpOptimizer::class => [],
-            Tool\AmpValidator::class => [],
-            Tool\Lighthouse::class   => [],
-        ];
+        // TODO: Dispatch event to allow for last-minute dynamic changes.
+
+        return $this->tools;
+    }
+
+    /**
+     * Register a new tool with the tool stack configuration.
+     *
+     * @param class-string<Tool>        $toolFqcn         Fully-qualified class name of the tool implementation.
+     * @param array<class-string<Tool>> $toolDependencies Array of fully-qualified class names of the tool dependencies.
+     */
+    public function registerTool($toolFqcn, $toolDependencies)
+    {
+        $this->tools[$toolFqcn] = $toolDependencies;
     }
 }
--- tests/Engine/ToolStack/ToolStackConfigurationTest.php
+++ tests/Engine/ToolStack/ToolStackConfigurationTest.php
@@ -2,6 +2,9 @@
 
 namespace PageExperience\Tests\Engine\ToolStack;
 
+use PageExperience\Engine\Tool\AmpOptimizer;
+use PageExperience\Engine\Tool\AmpValidator;
+use PageExperience\Engine\Tool\Lighthouse;
 use PageExperience\Engine\ToolStack\ToolStackConfiguration;
 use PageExperience\Tests\TestCase;
 
@@ -18,5 +21,44 @@ final class ToolStackConfigurationTest extends TestCase
         $toolStackConfiguration = new ToolStackConfiguration();
 
         self::assertInstanceOf(ToolStackConfiguration::class, $toolStackConfiguration);
+    }
+
+    public function testItCanReturnToolConfiguration()
+    {
+        $toolStackConfiguration = new ToolStackConfiguration();
+
+        $tools = $toolStackConfiguration->getTools();
+
+        self::assertIsArray($tools);
+        self::assertCount(3, $tools);
+        self::assertArrayHasKey(AmpValidator::class, $tools);
+        self::assertEquals([], $tools[AmpValidator::class]);
+        self::assertArrayHasKey(AmpOptimizer::class, $tools);
+        self::assertEquals([], $tools[AmpOptimizer::class]);
+        self::assertArrayHasKey(Lighthouse::class, $tools);
+        self::assertEquals([], $tools[Lighthouse::class]);
+    }
+
+    public function testItCanRegisterNewTools()
+    {
+        $toolStackConfiguration = new ToolStackConfiguration();
+
+        $toolStackConfiguration->registerTool('DummyToolA', []);
+        $toolStackConfiguration->registerTool('DummyToolB', [AmpValidator::class, 'DummyToolA']);
+
+        $tools = $toolStackConfiguration->getTools();
+
+        self::assertIsArray($tools);
+        self::assertCount(5, $tools);
+        self::assertArrayHasKey(AmpValidator::class, $tools);
+        self::assertEquals([], $tools[AmpValidator::class]);
+        self::assertArrayHasKey(AmpOptimizer::class, $tools);
+        self::assertEquals([], $tools[AmpOptimizer::class]);
+        self::assertArrayHasKey(Lighthouse::class, $tools);
+        self::assertEquals([], $tools[Lighthouse::class]);
+        self::assertArrayHasKey('DummyToolA', $tools);
+        self::assertEquals([], $tools['DummyToolA']);
+        self::assertArrayHasKey('DummyToolB', $tools);
+        self::assertEquals([AmpValidator::class, 'DummyToolA'], $tools['DummyToolB']);
     }
 }
